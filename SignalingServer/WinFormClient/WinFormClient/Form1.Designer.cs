a6f450ff75edd44cad44d500c9aaaea8c5cb1f7c
daesung <daesung@mediaweb.co.kr>
2025-05-23 17:23:27 +0900

    node_test

---
+++ SignalingServer/WinFormClient/WinFormClient/Form1.Designer.cs
@@ -0,0 +1,104 @@
+﻿
+namespace WinFormClient
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.Send = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.webView21 = new Microsoft.Web.WebView2.WinForms.WebView2();
+            ((System.ComponentModel.ISupportInitialize)(this.webView21)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.textBox1.Location = new System.Drawing.Point(0, 0);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(800, 21);
+            this.textBox1.TabIndex = 0;
+            // 
+            // Send
+            // 
+            this.Send.Dock = System.Windows.Forms.DockStyle.Top;
+            this.Send.Location = new System.Drawing.Point(0, 21);
+            this.Send.Name = "Send";
+            this.Send.Size = new System.Drawing.Size(800, 23);
+            this.Send.TabIndex = 1;
+            this.Send.Text = "메시지 보내기";
+            this.Send.UseVisualStyleBackColor = true;
+            this.Send.Click += new System.EventHandler(this.Send_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.textBox2.Location = new System.Drawing.Point(0, 320);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(800, 130);
+            this.textBox2.TabIndex = 2;
+            // 
+            // webView21
+            // 
+            this.webView21.AllowExternalDrop = true;
+            this.webView21.CreationProperties = null;
+            this.webView21.DefaultBackgroundColor = System.Drawing.Color.White;
+            this.webView21.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.webView21.Location = new System.Drawing.Point(0, 44);
+            this.webView21.Name = "webView21";
+            this.webView21.Size = new System.Drawing.Size(800, 276);
+            this.webView21.TabIndex = 3;
+            this.webView21.ZoomFactor = 1D;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.webView21);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.Send);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.webView21)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button Send;
+        private System.Windows.Forms.TextBox textBox2;
+        private Microsoft.Web.WebView2.WinForms.WebView2 webView21;
+    }
+}
+
---
+++ SignalingServer/WinFormClient/WinFormClient/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace WinFormClient
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            Init();
+        }
+            
+        private async void Init()
+        {
+            await webView21.EnsureCoreWebView2Async(null);
+            webView21.CoreWebView2.WebMessageReceived += CoreWebView2_WebMessageReceived;
+
+            //경로에서 html 가져오기
+            /*
+            string sHtmlPath = string.Format("{0}\\webrtc.html", Application.StartupPath);
+            webView21.CoreWebView2.Navigate(sHtmlPath);
+            */
+
+            //리소스에서 html 가져오기
+            string sHtmlPath = Properties.Resources.webrtc;             
+            webView21.CoreWebView2.NavigateToString(sHtmlPath);
+        }
+
+        private void CoreWebView2_WebMessageReceived(object sender, Microsoft.Web.WebView2.Core.CoreWebView2WebMessageReceivedEventArgs e)
+        {
+            //웹페이지에서 전달받은 메시지
+            string strMessage = e.TryGetWebMessageAsString();
+
+            textBox2.AppendText($"[웹페이지에서 WinForm으로]: {strMessage}\r\n");
+        }
+
+        private void Send_Click(object sender, EventArgs e)
+        {
+            string strMessage = textBox1.Text;
+            if (!string.IsNullOrWhiteSpace(strMessage))
+            {
+                //웹페이지로 메시지 전달
+                string js = $"window.receiveFromForm({System.Text.Json.JsonSerializer.Serialize(strMessage)});";
+                webView21.CoreWebView2.ExecuteScriptAsync(js);
+
+                textBox2.AppendText($"[WinForm에서 웹페이지로]: {strMessage}\r\n");
+                textBox1.Clear();
+            }
+        }
+
+    }
+}
